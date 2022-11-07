cd4c0b8840e5b47ef5fd55698d6fd5fb92bbcfd0
gcfp <guillemcalaffp@ibf.cat>
2022-11-07 18:10:07 +0100

    Merge branch 'main' of https://github.com/gcfp/M04

--- A3.2-DML.sql
+++ A3.2-DML.sql
@@ -15,12 +15,22 @@ SELECT * FROM piso WHERE ascensor = 1 and garaje = 1 and metros_2 > 80;
 #Mostrar los pisos que no sean de Barcelona, ni Badalona, ni Castelldefels y valgan menos de 250000 €, todo ello ordenado alfabéticamente por Población.
 SELECT * FROM piso WHERE poblacion NOT IN ('Barcelona', 'Badalona', 'Castelldefels') and precio < 250000;
 #Mostrar los pisos construidos a partir del año 2016.
+SELECT * FROM piso WHERE fecha_construccion >= '2016-01-01';
 #Mostrar los pisos que no sean de Barcelona, ni de Madrid, ni de Vic.
+SELECT * FROM piso WHERE poblacion NOT IN ('Barcelona', 'Madrid', 'Vic');
 #Mostrar la dirección y el 10% del precio de los pisos de Barcelona.
+SELECT direccion, precio*0.1 AS '10% precio' FROM piso Where poblacion = 'Barcelona';
 #Mostrar las poblaciones en las que tenemos pisos, no puede haber poblaciones repetidas.
+SELECT poblacion FROM piso GROUP BY poblacion;
 #Mostrar los pisos que estén en una Rambla.
+SELECT * FROM piso WHERE direccion LIKE '%Rambla%';
 #Mostrar los 5 pisos más caros.
+SELECT * FROM piso ORDER BY precio DESC LIMIT 5;
 #Borra los pisos que tengan 3 o menos habitaciones.
+DELETE FROM piso WHERE numero_habitaciones <= 3;
 #Borra los datos de la tabla.
+DELETE FROM piso;
 #Borra la tabla.
+DROP TABLE piso;
 #Borra la base de datos.
+DROP DATABASE PISOS;
